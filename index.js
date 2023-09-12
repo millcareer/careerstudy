f877845eac1c7fb33bb6f9fcb630a804e0dcfd75
millcareer <129133100+millcareer@users.noreply.github.com>
2023-09-13 01:08:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let yearSelect = document.getElementById('form_answer07');
-    let monthSelect = document.getElementById('form_answer08');
-    let daySelect = document.getElementById('form_answer09');
-
-    for(let i = 1900; i <= new Date().getFullYear(); i++) {
-        let option = document.createElement('option');
-        option.value = i;
-        option.text = i;
-        yearSelect.add(option);
-    }
-
-    for(let i = 1; i <= 12; i++) {
-        let option = document.createElement('option');
-        option.value = i;
-        option.text = i;
-        monthSelect.add(option);
-    }
-
-    for(let i = 1; i <= 31; i++) {
-        let option = document.createElement('option');
-        option.value = i;
-        option.text = i;
-        daySelect.add(option);
-    }
-});
-
-
-let formInputValues = [];  // 名前を変更して、何を格納しているかを明確に示します。
-let isFormValid = true;    // 名前を変更して、目的をより明確に示します。
+let formInputValues = [];  
+let isFormValid = true;    
 
 function collectFormValues() {
-    formInputValues = [];  // 関数が呼ばれるたびに配列をリセットします。
+    formInputValues = [];  
     for(let i = 1; i <= 19; i++) {
         formInputValues.push(document.getElementById(`form_answer${i.toString().padStart(2, '0')}`).value);
     }
 }
 
 function checkFormValidity() {
-    // ここでフォームの検証を行います。
-    // 例: 空の入力値がある場合、isFormValidをfalseに設定します。
     isFormValid = formInputValues.every(value => value.trim() !== '');
 }
 
@@ -47,7 +17,7 @@ function onSubmit() {
     checkFormValidity();
     
     if (isFormValid) {
-        window.sendMessageToLine(formInputValues.join(', ')); // formInputValuesをカンマで結合して送信します
+        window.sendMessageToLine(formInputValues.join(', ')); 
     } else {
         window.alert("フォームに無効な入力があります。");
     }
@@ -59,15 +29,10 @@ document.getElementById('submit-button').addEventListener('click', onSubmit);
  * ここから下は生年月日のためのjs
 */
 
-let userBirthdayYear = document.querySelector('.birthday-year');
-let userBirthdayMonth = document.querySelector('.birthday-month');
-let userBirthdayDay = document.querySelector('.birthday-day');
+let userBirthdayYear = document.getElementById('form_answer07');
+let userBirthdayMonth = document.getElementById('form_answer08');
+let userBirthdayDay = document.getElementById('form_answer09');
 
-/**
- * selectのoptionタグを生成するための関数
- * @param {Element} elem 変更したいselectの要素
- * @param {Number} val 表示される文字と値の数値
- */
 function createOptionForElements(elem, val) {
   let option = document.createElement('option');
   option.text = val;
@@ -88,17 +53,9 @@ for(let i = 1; i <= 31; i++) {
   createOptionForElements(userBirthdayDay, i);
 }
 
-/**
- * 日付を変更する関数
- */
 function changeTheDay() {
-  //日付の要素を削除
   userBirthdayDay.innerHTML = '';
-
-  //選択された年月の最終日を計算
   let lastDayOfTheMonth = new Date(userBirthdayYear.value, userBirthdayMonth.value, 0).getDate();
-
-  //選択された年月の日付を生成
   for(let i = 1; i <= lastDayOfTheMonth; i++) {
     createOptionForElements(userBirthdayDay, i);
   }
