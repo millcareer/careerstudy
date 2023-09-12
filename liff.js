990ef9876a6a5ad77e8bd667e9137fe9e03a8220
millcareer <129133100+millcareer@users.noreply.github.com>
2023-09-12 14:49:07 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,61 +1,59 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1660795452-nYx391B8";
-    console.log(`init liff, ID : ${liffId}`);
-    initializeLiff(liffId);
-})
+    const LIFF_ID = process.env.LIFF_ID || "YOUR_LIFF_ID_HERE";  // 環境変数からLIFF_IDを取得し、もしくはデフォルトの値を設定します。
 
 function initializeLiff(liffId) {
     liff
-        .init({
-            liffId: liffId
-        })
+        .init({ liffId })
         .then(() => {
-            // Webブラウザからアクセスされた場合は、LINEにログインする
-            if (!liff.isInClient() && !liff.isLoggedIn()) {
-                window.alert("LINEアカウントにログインしてください。");
-                liff.login({redirectUri: location.href});
-            }else{
-                console.log('Login Success');
-            }
+            handleLiffInitializationSuccess();
         })
         .catch((err) => {
-            console.log('LIFF Initialization failed ', err);
+            handleLiffInitializationFailure(err);
         });
 }
 
-function sendText(text) {
-    if (!liff.isInClient()) {
-        shareTargetPicker(text);
+function handleLiffInitializationSuccess() {
+    if (!liff.isInClient() && !liff.isLoggedIn()) {
+        window.alert("LINEアカウントにログインしてください。");
+        liff.login({ redirectUri: location.href });
     } else {
-        sendMessages(text);
+        console.log('Login Success');
     }
 }
 
-
-// LINEトーク画面上でメッセージ送信
-function sendMessages(text) {
-    console.log('in sendMessages()');
-    liff.sendMessages([{
-        'type': 'text',
-        'text': text
-    }]).then(function () {
-        liff.closeWindow();
-    }).catch(function (error) {
-        window.alert('Failed to send message ' + error);
-    });
+function handleLiffInitializationFailure(err) {
+    console.error('LIFF Initialization failed: ', err);
+    window.alert('アプリケーションの初期化に失敗しました。後ほど再試行してください。');
 }
 
-// Webブラウザからメッセージ送信
-function shareTargetPicker(text) {
-    console.log('in shareTargetPicker');
-    liff.shareTargetPicker([{
-        'type': 'text',
-        'text': text
-    }]).catch((error) => {
-        console.log(error);
-        window.alert('Failed to send message ' + error);
-    });
+function sendMessageToLine(text) {
+    if (!liff.isInClient()) {
+        openShareTargetPicker(text);
+    } else {
+        sendTextMessage(text);
+    }
+}
+
+function sendTextMessage(text) {
+    console.log('Sending message...');
+    liff.sendMessages([{ type: 'text', text }])
+        .then(() => {
+            liff.closeWindow();
+        })
+        .catch((error) => {
+            console.error('Failed to send message: ', error);
+            window.alert('メッセージの送信に失敗しました。');
+        });
+}
+
+function openShareTargetPicker(text) {
+    console.log('Opening Share Target Picker...');
+    liff.shareTargetPicker([{ type: 'text', text }])
+        .catch((error) => {
+            console.error('Failed to open Share Target Picker: ', error);
+            window.alert('シェアターゲットピッカーの開始に失敗しました。');
+        });
 }
 
