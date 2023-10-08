f082bdc7d59227d100a6b469be7f4918faf6c7eb
kobuke tomohiro <89505842+kobuke@users.noreply.github.com>
2023-10-08 09:22:32 +0200

    onSubmit関数を元の状態に戻す

--- index.js
+++ index.js
@@ -38,4 +38,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function onSubmit() {
     // Your existing onSubmit function here
+    let text_list = [];
+    text_list.push(document.getElementById('form_answer01').value);
+    text_list.push(document.getElementById('form_answer02').value);
+    text_list.push(document.getElementById('form_answer03').value);
+    text_list.push(document.getElementById('form_answer04').value);
+    text_list.push(document.getElementById('form_answer05').value);
+    text_list.push(document.getElementById('form_answer06').value);
+    text_list.push(document.getElementById('form_answer07').value);
+    text_list.push(document.getElementById('form_answer08').value);
+    text_list.push(document.getElementById('form_answer09').value);
+    text_list.push(document.getElementById('form_answer10').value);
+    text_list.push(document.getElementById('form_answer11').value);
+    text_list.push(document.getElementById('form_answer12').value);
+    text_list.push(document.getElementById('form_answer13').value);
+    text_list.push(document.getElementById('form_answer14').value);
+    text_list.push(document.getElementById('form_answer15').value);
+    text_list.push(document.getElementById('form_answer16').value);
+    text_list.push(document.getElementById('form_answer17').value);
+    text_list.push(document.getElementById('form_answer18').value);
+    text_list.push(document.getElementById('form_answer19').value);
+    
+    let msg = "【送信内容】";
+    let form_check_flag = 1;
+    for (let i = 0; i < text_list.length; i++) {
+        if ( text_list[i] == "") {
+            form_check_flag = 0;
+            window . alert( '入力項目に漏れがあります。全ての項目への入力をお願い致します。' );
+            break;
+        }
+        msg = msg + "\n" + text_list[i];
+    }
+    if (form_check_flag == 1) {
+        sendText(msg);
+    }
+    return false;
 }
