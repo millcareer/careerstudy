3c3b6ff7d7386a549ff14bf420ec1789aa8b91ec
kobuke tomohiro <89505842+kobuke@users.noreply.github.com>
2023-10-08 09:40:57 +0200

    元のjsの状態に戻す

--- liff.js
+++ liff.js
@@ -1,17 +1,54 @@
-function sendMessageToLine(text) {
-    if (!liff.isInClient()) {
-        openShareTargetPicker(text);
-    } else {
-        sendTextMessage(text);
-    }
-}
-window.sendMessageToLine = sendMessageToLine;
-
+// LIFFの基本情報
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    const LIFF_ID = "1660795452-nYx391B8";
-    initializeLiff(LIFF_ID);
+    var liffId = "1660795452-nYx391B8";
+    console.log(`init liff, ID : ${liffId}`);
+    initializeLiff(liffId);
+})
+
+// index.htmlのsubimtで呼び出される関数
+function sendText(text) {
+    if (!liff.isInClient()) {
+        shareTargetPicker(text);
+    } else {
+        sendMessages(text);
+    }
+}
+
+// LINEトーク画面上でメッセージ送信
+function sendMessages(text) {
+    console.log('in sendMessages()');
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {
+        liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
+
+// Webブラウザからメッセージ送信
+function shareTargetPicker(text) {
+    console.log('in shareTargetPicker');
+    liff.shareTargetPicker([{
+        'type': 'text',
+        'text': text
+    }]).catch((error) => {
+        console.log(error);
+        window.alert('Failed to send message ' + error);
+    });
+}
+
+// function sendMessageToLine(text) {
+//     if (!liff.isInClient()) {
+//         openShareTargetPicker(text);
+//     } else {
+//         sendTextMessage(text);
+//     }
+// }
+// window.sendMessageToLine = sendMessageToLine;
 
 function initializeLiff(liffId) {
     liff
@@ -38,24 +75,24 @@ function handleLiffInitializationFailure(err) {
     window.alert('アプリケーションの初期化に失敗しました。後ほど再試行してください。');
 }
 
-function sendTextMessage(text) {
-    console.log('Sending message...');
-    liff.sendMessages([{ type: 'text', text }])
-        .then(() => {
-            liff.closeWindow();
-        })
-        .catch((error) => {
-            console.error('Failed to send message: ', error);
-            window.alert('メッセージの送信に失敗しました。');
-        });
-}
+// function sendTextMessage(text) {
+//     console.log('Sending message...');
+//     liff.sendMessages([{ type: 'text', text }])
+//         .then(() => {
+//             liff.closeWindow();
+//         })
+//         .catch((error) => {
+//             console.error('Failed to send message: ', error);
+//             window.alert('メッセージの送信に失敗しました。');
+//         });
+// }
 
-function openShareTargetPicker(text) {
-    console.log('Opening Share Target Picker...');
-    liff.shareTargetPicker([{ type: 'text', text }])
-        .catch((error) => {
-            console.error('Failed to open Share Target Picker: ', error);
-            window.alert('シェアターゲットピッカーの開始に失敗しました。');
-        });
-}
+// function openShareTargetPicker(text) {
+//     console.log('Opening Share Target Picker...');
+//     liff.shareTargetPicker([{ type: 'text', text }])
+//         .catch((error) => {
+//             console.error('Failed to open Share Target Picker: ', error);
+//             window.alert('シェアターゲットピッカーの開始に失敗しました。');
+//         });
+// }
 
