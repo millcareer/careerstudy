d95b52c06be77340b2be4b34affe08c98abb47d4
millcareer <129133100+millcareer@users.noreply.github.com>
2024-04-17 19:58:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,8 @@ function onSubmit() {
 
 //スプレッドシートから選択肢を取得し動的にテキストボックスへ追加(2024年4月17日追加)
 document.addEventListener('DOMContentLoaded', function() {
-  const url = 'https://script.google.com/macros/s/AKfycbyiPXSL7S8YiUM5ZYSZBdigPsK8mOZ0DrKtXsznbkPyHNpVbvnZ4ASOlT98gjn6oEdZlQ/exec';
+  const url = 'http://localhost:3000/api'; // ローカルのプロキシサーバーを指定
+
 
   fetch(url)
     .then(response => response.json())
