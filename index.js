ae0b432e1b6719f85dcbb39e9ee9d1bd5a4355ba
millcareer <129133100+millcareer@users.noreply.github.com>
2024-09-17 11:10:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function onSubmit() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const url = 'https://script.google.com/macros/s/AKfycbwPyGgy-Vk9TB0cOe2s-rIWjElj9q5K3pO-pKNPndcA09ecYmmFhpa2Kog6OF3WHkug8g/exec'; // 日程取得用のウェブアプリURL
+    const url = 'https://script.google.com/macros/s/AKfycbze0vBfKEmpZq8vKtnrRtl8U1DWNgjQeQM8fwhhVYUTHqdy4ALG74x8eumT622MD5X7bQ/exec'; // 日程取得用のウェブアプリURL
 
     const radioGroup = document.getElementById('radio_group'); // 1つのラジオグループを使用
 
