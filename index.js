4f1d2a0365df3b57d410d688fe8c10912f448d6a
millcareer <129133100+millcareer@users.noreply.github.com>
2025-05-02 13:11:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function setupBirthdaySelects() {
 
 // APIエンドポイント
 // 注意: 実際のGCPエンドポイントURLに置き換える必要があります
-const API_ENDPOINT = "backend-sa@millcareer-app.iam.gserviceaccount.com";
+const API_ENDPOINT = "https://backend-service-694729750061.asia-northeast1.run.app";
 
 // フォーム送信関数
 function onSubmit() {
